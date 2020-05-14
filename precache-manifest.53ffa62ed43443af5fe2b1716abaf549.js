8f987bee06528563fabbc44121d48c4032ff8ded
mysore123 <apmysore10@gmail.com>
2020-05-15 03:45:20 +0530

    Updates

---
+++ precache-manifest.53ffa62ed43443af5fe2b1716abaf549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6d9c6a05c86f5c1f158d9334d6a0a0",
+    "url": "/elecbits_intern/index.html"
+  },
+  {
+    "revision": "a591e5bdf744a13e34dc",
+    "url": "/elecbits_intern/static/css/main.54ade347.chunk.css"
+  },
+  {
+    "revision": "bc5a2b0674243ac8ff58",
+    "url": "/elecbits_intern/static/js/2.576c1146.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/elecbits_intern/static/js/2.576c1146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a591e5bdf744a13e34dc",
+    "url": "/elecbits_intern/static/js/main.a3472e25.chunk.js"
+  },
+  {
+    "revision": "ada5a8f97601ad0457a1",
+    "url": "/elecbits_intern/static/js/runtime-main.f6be209d.js"
+  }
+]);
