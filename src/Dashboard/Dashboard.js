3636c38cb415af94b9c70853a6aeb2ba63bbf141
mysore123 <apmysore10@gmail.com>
2020-05-25 23:19:54 +0530

    Update Dashboard.js

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import Box from '@material-ui/core/Box';
@@ -30,6 +30,9 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import EqualizerIcon from '@material-ui/icons/Equalizer';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import Stackedchart from '../BottomfourBoxes/StackedChart';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+
 
 function Copyright() {
   return (
@@ -112,6 +115,46 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
   },
+
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+
   paper: {
     padding: theme.spacing(2),
     display: 'flex',
@@ -159,9 +202,22 @@ export default function Dashboard() {
           <Typography component="h1" variant="h4" color="inherit" noWrap className="title">
             Sachait
           </Typography>
-           
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+           </div>
           <IconButton color="inherit">
           </IconButton>
+         
         </Toolbar>
       </AppBar>
       <Drawer
@@ -184,7 +240,7 @@ export default function Dashboard() {
         <div className={classes.appBarSpacer} />
             <div >
                 <Typography variant="p" className="dash">Dashboard</Typography>
-                <Typography variant="subtitle2" className="welcome">Welcome to Ubold Admin Panel!</Typography>
+                
             </div>
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={1}>
@@ -243,7 +299,7 @@ export default function Dashboard() {
               </Paper>
             </Grid>
 
-            {/* Total revenue*/}
+            {/* Total revenue*
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}>
                 <Typography color="primary">Total Revenue</Typography>
@@ -252,6 +308,7 @@ export default function Dashboard() {
                 < Doughnut/>
               </Paper>
             </Grid>
+            */}
 
 
              {/* Chart */}
